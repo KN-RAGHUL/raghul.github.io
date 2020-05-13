80a568ea885a3d9d645fb5f0ca2d091277b345f0
KN-RAGHUL <massraghul1212@gmail.com>
2020-05-13 22:30:58 +0530

    update the files

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
-var value1 = document.getElementById("left_value").value;
-var value2 = document.getElementById("right_value").value;
+var value1 = document.getElementById("left_value");
+var value2 = document.getElementById("right_value");
 var backimage = document.getElementsByTagName("html")[0];
 var text = document.getElementById("value_show");
 var button = document.getElementById("changecolor");
-// var count;
-// count = 0;
 
 button.addEventListener("click", myfuntion);
 function myfuntion()
-{
-    // count++;
-    // text.textContent = count;
-    backimage.style.backgroundImage = 'linear-gradient('+value1+','+value2+')';
-    text.textContent = "first value"+value1+" second value"+value2;
+{    
+    backimage.style.backgroundImage = 'linear-gradient( to right,'+value1.value+','+value2.value+')';
+    text.textContent = "first value"+value1.value+" second value"+value2.value;
+    setTimeout(
+        function (){
+            location.reload();
+        },10000
+    );
 }
