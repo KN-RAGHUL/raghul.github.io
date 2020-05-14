f5c351d464d108f14536103ddee04f7d595e1e65
KN-RAGHUL <massraghul1212@gmail.com>
2020-05-14 12:38:02 +0530

    changes

--- javascript.js
+++ javascript.js
@@ -2,16 +2,11 @@ var value1 = document.getElementById("left_value");
 var value2 = document.getElementById("right_value");
 var backimage = document.getElementsByTagName("html")[0];
 var text = document.getElementById("value_show");
-var button = document.getElementById("changecolor");
 
-button.addEventListener("click", myfuntion);
+value1.addEventListener("input", myfuntion);
+value2.addEventListener("input", myfuntion);
 function myfuntion()
 {    
     backimage.style.backgroundImage = 'linear-gradient( to right,'+value1.value+','+value2.value+')';
     text.textContent = "first value"+value1.value+" second value"+value2.value;
-    setTimeout(
-        function (){
-            location.reload();
-        },10000
-    );
 }
